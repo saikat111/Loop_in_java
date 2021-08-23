6126752b6599c375bc02c92e5357db84a1c44c59
saikat111 <s8856010@gmail.com>
2021-08-23 22:01:55 +0600

    /for and while /

---
+++ src/DemoLoop.java
@@ -0,0 +1,72 @@
+/*
+    For Loop
+*/
+public class DemoLoop {
+    public static void main(String[] args) {
+        /*
+         * staring point
+         * end point
+         * i++
+         * */
+/*
+        System.out.println("i" + " " + "j");
+*/
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                System.out.println(i + " " + j);
+            }
+
+            /*
+             * I J
+             * 1 1
+             * 1 2
+             * 1 3
+             * 1 4
+             * 1 5
+             * 2 1
+             * 2 2
+             * 2 3
+             * 2 4
+             * 2 5
+             * 3 1
+             * --------
+             * 5 1
+             * 5 2
+             * 5 3
+             * 5 4
+             * 5 5
+             *
+             *
+             * */
+            /*
+             for(int i=1;i<=5;i++){
+            }
+            for i = 1
+            ---------
+            * i=1 , i = i+1  =  1 + 1 = 2
+            * i=2 ,          = 2 + 1 = 3
+            * i=3 ,          = 3 + 1 = 4
+            * i=4,
+            *  */
+
+            /*
+
+            for i = 2
+            ---------
+            * i=2 , i = i+1  =  2 + 1 = 3
+            * i=3 ,          = 3 + 1 = 4
+            * i=4,
+            *  */
+        /*int l;
+        for ( l= 1;l <=10; l++){
+            System.out.println(l);
+        }*/
+
+        /*int l =0;
+        for ( ;; l++){
+            System.out.println(l);
+        }
+*/
+        }
+    }
+}
